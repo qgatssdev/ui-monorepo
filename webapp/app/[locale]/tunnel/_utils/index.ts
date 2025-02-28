ccbaf1696b7349f74158d781fc7d3caf02faded9
qgatssdev <chivoomodu@gmail.com>
2025-02-28 21:02:10 +0100

    Add minimum amount check

--- webapp/app/[locale]/tunnel/_utils/index.ts
+++ webapp/app/[locale]/tunnel/_utils/index.ts
@@ -14,6 +14,9 @@ type CanSubmit = Pick<
   fromInput: string
 }
 
+export const getMinimumAmount = (token: Token): Big =>
+  new Big(1).div(new Big(10).pow(token.decimals))
+
 export const canSubmit = ({
   balance,
   chainId,
@@ -23,6 +26,8 @@ export const canSubmit = ({
 }: CanSubmit) =>
   Big(fromInput).gt(0) &&
   chainId === fromNetworkId &&
+  // amount must be at least the minimum representable value
+  Big(fromInput).gte(getMinimumAmount(fromToken)) &&
   // for native tokens, it can't match the whole balance
   // as native tokens are used to pay for fees
   Big(fromInput)[isNativeToken(fromToken) ? 'lt' : 'lte'](
